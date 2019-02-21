33f48eedcf2d086cb594772df8b2421d6c06821b
AmitLevi1004 <47784073+AmitLevi1004@users.noreply.github.com>
2019-02-21 03:33:11 -0800

    Update Main.java

--- Main.java
+++ Main.java
@@ -10,7 +10,7 @@ import javax.swing.JPanel;
 public class Main {
 	public static void main(String[] args) {
 		JFrame frame = new JFrame("Calculator");
-		frame.setSize(400, 200);
+		frame.setSize(800, 200);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		Calculator c = new Calculator();
 		frame.add(c);
