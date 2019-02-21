a8cdcf68f8d6839f41277662aa032261e40c5b66
AmitLevi1004 <47784073+AmitLevi1004@users.noreply.github.com>
2019-02-21 13:12:40 +0200

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,19 @@
+
+/*
+ * This program represents calculator
+ */
+
+import java.awt.Panel;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class Main {
+	public static void main(String[] args) {
+		JFrame frame = new JFrame("Calculator");
+		frame.setSize(400, 200);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		Calculator c = new Calculator();
+		frame.add(c);
+		frame.setVisible(true);
+	}
+}
